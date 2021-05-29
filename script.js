9617a698b1662eed7b1156b907fecea75b68e201
Ayush Chugh <ayushchugh2006@gmail.com>
2021-05-29 17:40:07 +0530

    close account

--- script.js
+++ script.js
@@ -188,3 +188,21 @@ btnTransfer.addEventListener('click', e => {
   }
 });
 
+btnClose.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount?.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    
+    // delete account
+    accounts.splice(index, 1);
+
+    // hide ui
+    containerApp.style.opacity = 0;
+  }
+});
